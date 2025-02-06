a891ad8b58e5219f1607beb23bee0765de1d4d6b
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-02-06 07:02:46 +0100

    I think i fixed stuff.

--- angelos_bot.py
+++ angelos_bot.py
@@ -1,35 +1,34 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
-    print(f'The bot has finished setting up as {bot.user}')
+    print(f'We have logged in as {bot.user}')
 
 @bot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.text_channels, name='general')
     if channel is not None:
-        await channel.send(f'Hello {member.mention}, welcome to Los Angelos Roleplay!')
-        
+        await channel.send(f'Hello {member.mention}, welcome to the server!')
+
 @bot.event
 async def on_member_remove(member):
     channel = discord.utils.get(member.guild.text_channels, name='general')
     if channel is not None:
         await channel.send(f'{member.mention} didn\'t enjoy his stay :(')
-        
-@bot.command()
-async def announce(ctx, *, message: str):
+
+@slash.slash(name="announce", description="Send an announcement to the announcements channel")
+async def announce(ctx: SlashContext, *, message: str):
     channel = discord.utils.get(ctx.guild.text_channels, name='announcements')
     if channel is not None:
         await channel.send(message)
+    await ctx.send(f"Announcement sent: {message}")
 
-
-
-token = "****************"
-
-bot.run(token)
+bot.run('****************') 
