cf6685633fc172ecf11dec03038f521c8a94e892
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-02-12 16:31:30 +0100

    Fixx

--- squid_bot.py
+++ squid_bot.py
@@ -188,7 +188,7 @@ async def suggest(interaction: discord.Interaction, suggestion: str):
         await channel.send(embed=embed, view=view)
         await interaction.response.send_message("Suggestion submitted!", ephemeral=True)
     else:
-        await interaction.response.send_message("Internal error: Channel not found.", ephemeral=True
+        await interaction.response.send_message("Internal error: Channel not found.", ephemeral=True)
 
       
       
