b67c50685a0f85d0c8bfe6938c4c1d6acfa8d23e
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-02-12 15:52:53 +0100

    Fix.

--- squid_bot.py
+++ squid_bot.py
@@ -100,7 +100,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="over the server"
+            name="Join Squid Squad Now!"
         )
     )
 
